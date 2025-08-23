2c2212c1187451165190991fd0ee96debdc71445
sieun2im <kse040531@naver.com>
2025-08-24 03:09:17 +0900

    sdfs

--- NewIssue/newIssue.js
+++ NewIssue/newIssue.js
@@ -19,46 +19,46 @@ async function getIssues() {
         }
 
         const data = await res.json();
-        console.log(data);
-        return data;
+
+        const createIssue = (issue) => {
+            const issues = document.createElement('div');
+            issues.classList.add('issues');
+
+            const issueImg = document.createElement('img');
+            // issueImg.setAttribute('src',);
+            issueImg.classList.add('issueImg');
+
+            const issueText = document.createElement('div');
+            issueText.classList.add('issueText');
+
+            const textH2 = document.createElement('h2');
+            textH2.textContent = issue.name;
+
+            const textH3 = document.createElement('h3');
+            textH3.textContent = issue.category;
+
+            const textP = document.createElement('p');
+            textP.textContent = "aaw";
+
+            issueText.appendChild(textH2);
+            issueText.appendChild(textH3);
+            issueText.appendChild(textP);
+            issues.appendChild(issueImg);
+            issues.appendChild(issueText);
+            issuesPage.appendChild(issues);
+        }
+
+
+        data.forEach(issue => {
+            createIssue(issue);
+        });
+
 
     } catch (error) {
         console.log(error)
     }
 }
 
-const issueList = getIssues();
 
 
-const createIssue = (issue) => {
-    const issues = document.createElement('div');
-    issues.classList.add('issues');
-
-    const issueImg = document.createElement('img');
-    // issueImg.setAttribute('src',);
-    issueImg.classList.add('issueImg');
-
-    const issueText = document.createElement('div');
-    issueText.classList.add('issueText');
-
-    const textH2 = document.createElement('h2');
-    textH2.textContent = issue.name;
-
-    const textH3 = document.createElement('h3');
-    textH3.textContent = issue.category;
-
-    const textP = document.createElement('p');
-    textP.textContent = "aaw";
-
-    issueText.appendChild(textH2);
-    issueText.appendChild(textH3);
-    issueText.appendChild(textP);
-    issues.appendChild(issueImg);
-    issues.appendChild(issueText);
-    issuesPage.appendChild(issues);
-}
-
-issueList.forEach(issue => {
-    createIssue(issue);
-});
 
